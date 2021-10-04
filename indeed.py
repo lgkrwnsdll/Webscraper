5bc550c18705829321b3036a32cf272a6d75ec05
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2021-10-04 23:02:33 +0900

    extracting links

--- indeed.py
+++ indeed.py
@@ -11,19 +11,30 @@ def extract_indeed_pages():
     max_page = int(pagination.find('b').text)
     return max_page
 
+def extract_job(html):
+    title = html.find('h2', {'class':'jobTitle'}).text
+    company = html.find('span', {'class':'companyName'})
+    if company is None:
+        company = ''
+    else:
+        company = company.text
+    location = html.find('div', {'class':'companyLocation'}).text
+    job_id = html['data-jk']
+    return {
+    'title':title, 
+    'company':company, 
+    'location':location, 
+    'link': f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50&vjk={job_id}"
+    }
+
 def extract_indeed_jobs(last_page):
     jobs=[]
     for page in range(last_page):
+        print(f'Scrapping page {page+1}')
         result = requests.get(f'{URL[:-5]}{page*LIMIT}')
         soup = BeautifulSoup(result.text, 'html.parser')
-        results = soup.find_all('table', {'class':'jobCard_mainContent'})
+        results = soup.find_all('a', {'class':'tapItem'})
         for result in results:
-            title = result.find('h2', {'class':'jobTitle'}).text
-            company = result.find('span', {'class':'companyName'})
-            if company is None:
-                company = ''
-            else:
-                company = company.text
-            location = result.find('div', {'class':'companyLocation'}).text
-            jobs.append({'title':title, 'company':company, 'location':location})
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
